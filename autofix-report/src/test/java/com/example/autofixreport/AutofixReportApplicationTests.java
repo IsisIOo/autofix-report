f606cd1c6ac50be29bb7fbf20df8c12b7bb925ef
Isidora <isidora.oyanedel@usach.cl>
2024-05-21 20:31:59 -0400

    iniciando

---
+++ autofix-report/src/test/java/com/example/autofixreport/AutofixReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autofixreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutofixReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
